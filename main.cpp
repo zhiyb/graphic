c35d37f7f0ff4a3d63685d8dcd83231929124b58
zhiyb <yz39g13@soton.ac.uk>
2014-10-30 22:05:57 +0000

    Testing

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(void)
 
 	// Generate arrays
 	float r = 0.8;
-	int cLines = 24;
+	int cLines = 36;
 	Vector3D center(0.f, 0.f, 0.2);
 	for (double a = 0; a <= 2.f * PI; a += PI * 2.f / (float)cLines) {
 		Vector3D p(r * cos(a), r * sin(a), 0);
@@ -31,14 +31,14 @@ int main(void)
 		display->colours().push_back(Vector3D());
 	}
 	//float r = 0.8;
-	int c = 24;
+	int c = 35;
 	display->vertices().push_back(Vector3D());
-	display->colours().push_back(Vector3D(1.f, 0.f, 0.f));
+	display->colours().push_back(Vector3D(0.f, 1.f, 0.f));
 	for (int i = 0; i <= c; i++) {
 		Vector3D p(r * cos((float)i * 2.f * PI / (float)c), r * sin((float)i * 2.f * PI / (float)c), 0);
 		display->vertices().push_back(Vector3D(p));
 		if (i % 2)
-			display->colours().push_back(Vector3D(0.f, 1.f, 0.f));
+			display->colours().push_back(Vector3D(1.f, 0.f, 0.f));
 		else
 			display->colours().push_back(Vector3D(0.f, 0.f, 1.f));
 	}
@@ -52,11 +52,13 @@ start:
 		display->setModelView(m);
 		for (int i = 0; i <= cLines; i++) {
 			float o = (float)((i + of) % cLines) / (float)cLines;
-			display->colour(i * 2) = Vector3D(o, 1.f - o, 0);
+			//display->colour(i * 2) = Vector3D(o, 1.f - o, 0);
+			display->colour(i * 2) = Vector3D(1.f - o, 0, 0);
+			//display->colour(i * 2 + 1) = Vector3D(1.f - o, 1.f - o, o);
 			display->colour(i * 2 + 1) = Vector3D(0, 0, o);
 		}
 		display->drawArray(Display::TriangleStrip, 0, cLines * 2 + 2);
-		display->drawArray(Display::TriangleFan, cLines * 2 + 2, of % (c + 2) + 1);
+		display->drawArray(Display::TriangleFan, cLines * 2 + 2, of % (c + 1) + 2);
 		display->update();
 		usleep(20000);
 	}
