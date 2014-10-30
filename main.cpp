914539205b19a6f39d8318f1a887c04d8d8b9a8b
zhiyb <yz39g13@soton.ac.uk>
2014-10-30 22:20:02 +0000

    Testing: a simple cube

--- main.cpp
+++ main.cpp
@@ -20,28 +20,27 @@ int main(void)
 	init();
 
 	// Generate arrays
-	float r = 0.8;
-	int cLines = 36;
-	Vector3D center(0.f, 0.f, 0.2);
-	for (double a = 0; a <= 2.f * PI; a += PI * 2.f / (float)cLines) {
-		Vector3D p(r * cos(a), r * sin(a), 0);
-		display->vertices().push_back(center + p / 3.f);
-		display->vertices().push_back(center + p);
-		display->colours().push_back(Vector3D());
-		display->colours().push_back(Vector3D());
-	}
-	//float r = 0.8;
-	int c = 35;
-	display->vertices().push_back(Vector3D());
-	display->colours().push_back(Vector3D(0.f, 1.f, 0.f));
-	for (int i = 0; i <= c; i++) {
-		Vector3D p(r * cos((float)i * 2.f * PI / (float)c), r * sin((float)i * 2.f * PI / (float)c), 0);
-		display->vertices().push_back(Vector3D(p));
-		if (i % 2)
-			display->colours().push_back(Vector3D(1.f, 0.f, 0.f));
-		else
-			display->colours().push_back(Vector3D(0.f, 0.f, 1.f));
-	}
+	const float coords[6][4][3] = {
+		{{+0.5, +0.5, +0.5}, {+0.5, -0.5, +0.5}, {+0.5, -0.5, -0.5}, {+0.5, +0.5, -0.5}},
+		{{-0.5, +0.5, -0.5}, {-0.5, +0.5, +0.5}, {+0.5, +0.5, +0.5}, {+0.5, +0.5, -0.5}},
+		{{-0.5, +0.5, +0.5}, {-0.5, -0.5, +0.5}, {+0.5, -0.5, +0.5}, {+0.5, +0.5, +0.5}},
+		{{-0.5, +0.5, -0.5}, {-0.5, -0.5, -0.5}, {-0.5, -0.5, +0.5}, {-0.5, +0.5, +0.5}},
+		{{-0.5, -0.5, +0.5}, {-0.5, -0.5, -0.5}, {+0.5, -0.5, -0.5}, {+0.5, -0.5, +0.5}},
+		{{+0.5, +0.5, -0.5}, {+0.5, -0.5, -0.5}, {-0.5, -0.5, -0.5}, {-0.5, +0.5, -0.5}},
+	};
+	const float colours[6][3] = {
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1},
+		{0, 1, 1},
+		{1, 0, 1},
+		{1, 1, 0},
+	};
+	for (int i = 0; i < 6; i++)
+		for (int j = 0; j < 4; j++) {
+			display->vertices().push_back(Vector3D(coords[i][j][0], coords[i][j][1], coords[i][j][2]));
+			display->colours().push_back(Vector3D(colours[i][0], colours[i][1], colours[i][2]));
+		}
 
 start:
 	int ro = 360;
@@ -50,15 +49,8 @@ start:
 		Matrix4x4 m;
 		m.rotate(PI * 2.f * (float)of / (float)ro, Vector3D(1, 1, 0));
 		display->setModelView(m);
-		for (int i = 0; i <= cLines; i++) {
-			float o = (float)((i + of) % cLines) / (float)cLines;
-			//display->colour(i * 2) = Vector3D(o, 1.f - o, 0);
-			display->colour(i * 2) = Vector3D(1.f - o, 0, 0);
-			//display->colour(i * 2 + 1) = Vector3D(1.f - o, 1.f - o, o);
-			display->colour(i * 2 + 1) = Vector3D(0, 0, o);
-		}
-		display->drawArray(Display::TriangleStrip, 0, cLines * 2 + 2);
-		display->drawArray(Display::TriangleFan, cLines * 2 + 2, of % (c + 1) + 2);
+		for (int i = 0; i < 6; i++)
+			display->drawArray(Display::TriangleFan, i * 4, 4);
 		display->update();
 		usleep(20000);
 	}
